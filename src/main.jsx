48ad8a12b5b9eee609e7c507104060abe431af9a
Daniel <daniel@thebox3d.com>
2024-01-13 14:14:56 -0700

    0.0.25
    Login fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,17 @@ import { store } from "./slices/Store.jsx"; // Adjust the import path as necessa
 import { Provider } from "react-redux";
 import Modal from "react-modal"; // Import Modal from react-modal
 import "react-toastify/dist/ReactToastify.css"; // Import Toastify styles
+import { redirect } from "react-router-dom";
 
 Modal.setAppElement("#root"); // Set the app element to the root element of your application
-const onRedirectCallback = (user, app_state) => {
-  console.log({ user, app_state });
+const onRedirectCallback = () => {
+  redirect("/");
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
   <KindeProvider
     clientId="ff3837a9ea2c48a4ab002a976ece680a"
-    domain="https://shemonindustries.kinde.com"
+    domain="https://account.3dlogbook.com"
     logoutUri="https://www.3dlogbook.com/"
     redirectUri="https://www.3dlogbook.com/"
     audience="https://thebox3d.com"
@@ -25,7 +25,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     isDangerouslyUseLocalStorage={true}
   >
     <Provider store={store}>
-    
       <App />
     </Provider>
   </KindeProvider>
