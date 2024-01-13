68c060b7c34dfb7ba59518d5a484194449632375
Daniel <117490095+dans2177@users.noreply.github.com>
2024-01-13 12:26:56 -0700

    0.0.22
    redirect fix

--- src/main.jsx
+++ src/main.jsx
@@ -17,8 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <KindeProvider
     clientId="ff3837a9ea2c48a4ab002a976ece680a"
     domain="https://shemonindustries.kinde.com"
-    logoutUri="http://localhost:5173/"
-    redirectUri="http://localhost:5173/"
+    logoutUri="https://www.3dlogbook.com/"
+    redirectUri="https://www.3dlogbook.com/"
     audience="https://thebox3d.com"
     onRedirectCallback={onRedirectCallback}
     //DO NOT USE BELOW IN PRODUCTION SET CUSTOM DOMAIN!!!!
